732853bdd2e18abdbb95fda6f9393a08b60869bf
Armin Bronja <42872130+BronjaA@users.noreply.github.com>
2019-05-04 17:06:18 +0200

    broxov login i start page

---
+++ Projekat/Projekat/UserControls/dodajNoviProjekat.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekat.UserControls
+{
+    /// <summary>
+    /// Interaction logic for dodajNoviProjekat.xaml
+    /// </summary>
+    public partial class dodajNoviProjekat : UserControl
+    {
+        public dodajNoviProjekat()
+        {
+            InitializeComponent();
+        }
+
+        private void Xdodajnoviprojekat_Click(object sender, RoutedEventArgs e)
+        {
+            pocetnaStranaUC psuc = new pocetnaStranaUC();
+            Grid parentform = (this.Parent as Grid);
+            parentform.Children.Remove(this);
+            parentform.Children.Add(psuc);
+        }
+
+        private void Odustani_Click(object sender, RoutedEventArgs e)
+        {
+            pocetnaStranaUC psuc = new pocetnaStranaUC();
+            Grid parentform = (this.Parent as Grid);
+            parentform.Children.Remove(this);
+            parentform.Children.Add(psuc);
+        }
+    }
+}
---
+++ Projekat/Projekat/UserControls/pocetnaStranaUC.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekat.UserControls
+{
+    /// <summary>
+    /// Interaction logic for pocetnaStranaUC.xaml
+    /// </summary>
+    public partial class pocetnaStranaUC : UserControl
+    {
+        public pocetnaStranaUC()
+        {
+            InitializeComponent();
+        }
+
+        private void DodajNoviProj_Click(object sender, RoutedEventArgs e)
+        {
+            dodajNoviProjekat dnp = new dodajNoviProjekat();
+            Grid parentform = (this.Parent as Grid);
+            parentform.Children.Remove(this);
+            parentform.Children.Add(dnp);
+        }
+    }
+}
