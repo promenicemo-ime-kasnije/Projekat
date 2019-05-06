3de102d94e02e445c14f95596b3582ecc0392a68
samedb <41544886+samedb@users.noreply.github.com>
2019-05-06 02:11:47 +0200

    ProjectPage povezan sa eventima

--- Projekat/Projekat/Pages/ProjectPage.xaml.cs
+++ Projekat/Projekat/Pages/ProjectPage.xaml.cs
@@ -1,18 +1,6 @@
-﻿using Projekat.Pomocne_klase;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Projekat.Pages
 {
@@ -47,7 +35,7 @@ namespace Projekat.Pages
             Frame.Content = new TroskoviPage();
         }
 
-        private void PrikazuTabelu_Click(object sender, RoutedEventArgs e)
+        private void PrikaziTabelu_Click(object sender, RoutedEventArgs e)
         {
             Frame.Content = new TabelaPage();
         }
@@ -57,13 +45,9 @@ namespace Projekat.Pages
             Frame.Content = new TimelinePage();
         }
 
-        private void BackToStartPage_Click(object sender, RoutedEventArgs e)
+        private void ComingSoon_Click(object sender, RoutedEventArgs e)
         {
-            string poruka = "Nisam ovo jos uradio kako treba, za sad kad se klikne ovde moze samo da se ide nazad na pocetnu stranu. Da li to hocete?";
-            var result = MessageBox.Show(poruka, "Pitanje", MessageBoxButton.YesNo, MessageBoxImage.Question);
-
-            if (result == MessageBoxResult.Yes)
-                (Parent as Window).Content = new StartPage();
+            MessageBox.Show("Coming soon!");
         }
     }
 }
