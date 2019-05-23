3917edc4a6e786541329a51ccb24758aa12eb365
samedb <41544886+samedb@users.noreply.github.com>
2019-05-23 23:55:36 +0200

    StartPage je sad TabbedPage i ima tabove Projekti, Aktivnosti i Zahtevi.  Dodat i meni gde moze da se ide u podesavanja, podesavanja naloga i gde moze da se odjavi

--- Projekat/MobileApp/MobileApp/StartPage.xaml.cs
+++ Projekat/MobileApp/MobileApp/StartPage.xaml.cs
@@ -10,41 +10,59 @@ using Xamarin.Forms.Xaml;
 namespace MobileApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class StartPage : ContentPage
+    public partial class StartPage : TabbedPage
     {
         public StartPage()
         {
             InitializeComponent();
-            MyListView.ItemsSource = new string[] { "Projekti", "Aktivnosti", "Zahtevi", "Podesavanja", "Odjavi se" };
+            //MyListView.ItemsSource = new string[] { "Projekti", "Aktivnosti", "Zahtevi", "Podesavanja", "Odjavi se" };
             BindingContext = this;
         }
 
-        private async void MyListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        private async void OnClick(object sender, EventArgs e)
         {
-            Page GoToPage = null;
-
-            switch (e.Item.ToString())
+            Page gotoPage = null;
+            switch ((sender as ToolbarItem).Text)
             {
-                case "Projekti":
-                    GoToPage = new OdabirProjektaPage();
-                    break;
-                case "Aktivnosti":
-                    GoToPage = new AktivnostiPage();
-                    break;
-                case "Zahtevi":
-                    GoToPage = new ZahteviPage();
-                    break;
                 case "Podesavanja":
-                    GoToPage = new PodesavanjaPage();
+                    gotoPage = new PodesavanjaPage();
+                    break;
+                case "Nalog":
+                    gotoPage = new GenericPage("Nalog Page");
                     break;
                 case "Odjavi se": // ova se malo razlikuje
-                    GoToPage = new LoginPage();
-                    App.Current.MainPage = GoToPage;
+                    App.Current.MainPage = new LoginPage();
                     return;
             }
-
-            MyListView.SelectedItem = null;
-            await Navigation.PushAsync(GoToPage);
+            await Navigation.PushAsync(gotoPage);
         }
+
+        //private async void MyListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        //{
+        //    Page GoToPage = null;
+
+        //    switch (e.Item.ToString())
+        //    {
+        //        case "Projekti":
+        //            GoToPage = new OdabirProjektaPage();
+        //            break;
+        //        case "Aktivnosti":
+        //            GoToPage = new AktivnostiPage();
+        //            break;
+        //        case "Zahtevi":
+        //            GoToPage = new ZahteviPage();
+        //            break;
+        //        case "Podesavanja":
+        //            GoToPage = new PodesavanjaPage();
+        //            break;
+        //        case "Odjavi se": // ova se malo razlikuje
+        //            GoToPage = new LoginPage();
+        //            App.Current.MainPage = GoToPage;
+        //            return;
+        //    }
+
+        //    MyListView.SelectedItem = null;
+        //    await Navigation.PushAsync(GoToPage);
+        //}
     }
 }
